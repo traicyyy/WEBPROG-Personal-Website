4f83707feba5ca0fba50f87bc5bd93a6594f3b5e
Tracie Tomon <tctomon@student.apc.edu.ph>
2024-12-16 16:33:12 +0800

    update

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -4,5 +4,5 @@ function myFunction() {
 
   function myFunction2() {
     const x = document.getElementsByClassName("intro");
-    document.getElementById("demo").innerHTML = 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+    document.getElementById("demo").innerHTML = 'Know all about me' + x[0].innerHTML;
   }
