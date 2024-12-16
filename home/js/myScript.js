ac0b3b7d7cd99850e525571dee5eb2cb5ba297e8
Tracie Tomon <tctomon@student.apc.edu.ph>
2024-12-16 16:22:34 +0800

    added new function

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+  function myFunction2() {
+    const x = document.getElementsByClassName("intro");
+    document.getElementById("demo").innerHTML = 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+  }
