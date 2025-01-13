0ca11c87182aa356c8fed9eddb3362388c04fb2c
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-13 10:57:41 +0800

    update

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -37,16 +37,18 @@ document.addEventListener('DOMContentLoaded', function () {
     sections.forEach(section => observer.observe(section));
 
     // Letter by letter animation
-    const text = document.getElementById('animated-text').textContent;
+    const text = document.getElementById('animated-text').innerHTML;
     const container = document.getElementById('animated-text');
-    container.textContent = '';
-    
+    container.innerHTML = '';
+
     text.split('').forEach((char, index) => {
         const span = document.createElement('span');
-        span.textContent = char;
+        if (char === '.') {
+            container.appendChild(document.createElement('br'));
+        }
+        span.innerHTML = char;
         span.style.animationDelay = `${index * 0.1}s`;
         span.classList.add('letter');
         container.appendChild(span);
-    
     });
 });
