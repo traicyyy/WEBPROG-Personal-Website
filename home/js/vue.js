9a26f874125a4cb7392b9fcb0034ec30e9496c6f
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-16 03:46:10 +0800

    update

---
+++ home/js/vue.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+     return {
+       manyFoods: [
+         {name: 'profile', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/profile.jpg'},
+         {name: 'classmates', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/classmates.jpg'},
+         {name: 'science majors', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/science majors.jpg'},
+         {name: 'experiment', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/experiment.jpg'},
+         {name: 'book', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/book.jpg'},
+         {name: 'family', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/family.jpg'},
+         {name: 'dodong', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/dodong.jpg'},
+         {name: 'friends', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/friends2.jpg'},
+         {name: 'food', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/food.jpg'},
+         {name: 'travel', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/travel.jpg'},
+         {name: 'one piece', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/one piece.jpg'},
+         {name: 'conan', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/conan.jpg'},
+         {name: 'solo leveling', url: 'https://raw.githubusercontent.com/traicyyy/WEBPROG-Personal-Website/feature/2_TCT_Customize_My_Website/home/images/solo leveling.jpg'}
+       ]
+     }
+    }
+   })
+   
+   app.mount('#app')
+ 
