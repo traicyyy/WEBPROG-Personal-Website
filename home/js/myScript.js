53dabdbb10d4cf3a6a849ccbb9c5b12a874985e5
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-16 00:46:19 +0800

    update

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -16,20 +16,25 @@ function goToFacebook() {
 }
 
 document.getElementById('luffy').addEventListener('click', function() {
+    console.log('Luffy clicked');
     showInfo('About Me: Hi! I am passionate about technology and love exploring new innovations. I enjoy coding and learning about different aspects of IT. My journey in tech has been exciting and full of challenges.');
 });
 
 document.getElementById('gojo').addEventListener('click', function() {
+    console.log('Gojo clicked');
     showInfo('IT Experience: I have worked on various projects, ranging from web development to data analysis. My expertise includes programming in languages like JavaScript, Python, and PHP, as well as using frameworks and tools such as React, Django, and Docker.');
 });
 
 document.getElementById('sungjinwoo').addEventListener('click', function() {
+    console.log('Sung Jinwoo clicked');
     showInfo('Hobbies and Interests: In my free time, I enjoy reading, hiking, and playing video games. I am also a fan of anime and manga, which inspire my creativity and provide a fun escape from everyday life.');
 });
 
 document.getElementById('conan').addEventListener('click', function() {
+    console.log('Conan clicked');
     showInfo('Goals and Dreams: My dream is to become a successful software engineer and contribute to meaningful projects that make a difference. I aim to continually learn and grow in the tech industry while pursuing my passion for innovation.');
 });
+
 
 function showInfo(text) {
     var infoBox = document.getElementById('info');
